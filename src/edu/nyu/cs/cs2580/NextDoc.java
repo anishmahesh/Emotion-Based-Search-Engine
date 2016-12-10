f3b2bfd12a8b19f6dc60a64a3c6e8107fbca0f92
namanksrivastava <namanksrivastava@gmail.com>
2016-12-10 01:58:40 -0500

    Next Doc class

---
+++ src/edu/nyu/cs/cs2580/NextDoc.java
@@ -0,0 +1,15 @@
+package edu.nyu.cs.cs2580;
+
+
+/**
+ * Created by naman on 12/9/2016.
+ */
+public class NextDoc {
+    boolean stopRepeat;
+    Document doc;
+
+    public NextDoc(boolean stopRepeat, Document doc){
+        this.stopRepeat = stopRepeat;
+        this.doc = doc;
+    }
+}
